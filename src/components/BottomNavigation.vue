fff14ed239a382455392497d68919d75c03ed63e
Lord Sayur A <umar_317@hotmail.com>
2019-12-07 17:49:25 +0800

    UI Component - create bottom navigation

---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-bottom-navigation
+    v-model="selectedDistrict"
+    app
+    horizontal
+    grow
+    background-color="blue-grey darken-2"
+    color="white"
+  >
+
+    <v-btn v-for="(district, index) in districts" :key="index" height="100%" :value="district.value">
+      <span v-html="district.name"></span>
+    </v-btn>
+
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    name: 'BottomNavigation',
+    data () {
+      return {
+        selectedDistrict: 'brunei',
+        districts: [
+          {
+            name: "Brunei Muara <br> Temburong",
+            value: 'brunei'
+          },
+          {
+            name: "Tutong",
+            value: 'tutong'
+          },
+          {
+            name: "Belait",
+            value: 'belait'
+          },
+        ]
+      }
+    },
+  }
+</script>
