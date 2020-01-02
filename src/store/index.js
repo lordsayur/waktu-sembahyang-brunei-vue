bb13fa5928b923265d0a3334de4e4e79ab59352e
Omar <umar_317@hotmail.com>
2020-01-02 11:14:17 +0800

    Vuex - index - import Prayers, Days and Months modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import prayersModule from "./module/Prayers";
+import monthsModule from "./module/Months";
+import daysModule from "./module/Days";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    prayers: prayersModule,
+    months: monthsModule,
+    days: daysModule
+  }
 });
