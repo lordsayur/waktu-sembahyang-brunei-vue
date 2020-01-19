a7a1590d876c58a679dcea326e0444558c4f8f0a
Omar <umar_317@hotmail.com>
2020-01-19 17:52:58 +0800

    Unit Test - create global mixin test

---
+++ tests/unit/global.spec.js
@@ -0,0 +1,40 @@
+import { shallowMount } from "@vue/test-utils";
+import global from "@/mixins/global.js";
+
+describe("wsbPrint", () => {
+  jest.spyOn(console, "log");
+  const Component = {
+    render() {},
+    mixins: [global]
+  };
+  const wrapper = shallowMount(Component);
+
+  wrapper.vm.wsbPrint("title", "message");
+  it("should display title and message", () => {
+    expect(console.log).toHaveBeenCalledWith("title", "message");
+  });
+
+  wrapper.vm.wsbPrint("title", { name: "nama" });
+  it("should display title and object", () => {
+    expect(console.log).toHaveBeenCalledWith(
+      "title",
+      `{
+    "name": "nama"
+}`
+    );
+  });
+
+  wrapper.vm.wsbPrint("message");
+  it("should display message", () => {
+    expect(console.log).toHaveBeenCalledWith("message", "");
+  });
+
+  wrapper.vm.wsbPrint({ name: "nama" });
+  it("should display object", () => {
+    expect(console.log).toHaveBeenCalledWith(
+      `{
+    "name": "nama"
+}`
+    );
+  });
+});
