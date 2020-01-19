303a9ee861aacb6b6c451bfe800a80eb44c1402c
Omar <umar_317@hotmail.com>
2020-01-19 14:54:49 +0800

    Admin - add form for uploading prayer data to database

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="about">
-    <h1>Admin</h1>
-  </div>
+  <v-container fluid>
+    <h1 class="text-white">Admin</h1>
+    <h2 class="text-white">Update Prayer Time Database</h2>
+
+    <v-select
+      v-model="selected_month"
+      outlined
+      label="Months"
+      :items="months"
+      :item-text="months.text"
+      :item-value="months.value"
+    ></v-select>
+
+    <v-textarea
+      outlined
+      label="Prayer time"
+      v-model="originalText"
+    ></v-textarea>
+
+    <v-btn color="primary" class="mx-2" @click="convertText">Convert</v-btn>
+    <v-btn color="primary" class="mx-2" @click="uploadData">Upload</v-btn>
+    <router-link to="/">
+      <v-btn color="primary" class="mx-1">Home</v-btn>
+    </router-link>
+
+    <ol>
+      <li v-for="(line, index) in result" :key="index">
+        <ul>
+          <li v-for="(value, key) in line" :key="key">
+            {{ key }} - {{ value }}
+          </li>
+        </ul>
+      </li>
+    </ol>
+
+    <v-snackbar v-model="isUploaded">
+      Upload successful
+      <v-btn color="pink" text @click="isUploaded = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
