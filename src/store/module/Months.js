a4f22f39d67c250370206671de28f31da6b0ae1a
Omar <umar_317@hotmail.com>
2020-01-19 14:32:57 +0800

    Vuex - Month module - change months array to object with value added and get month object getters

--- src/store/module/Months.js
+++ src/store/module/Months.js
@@ -2,32 +2,36 @@ export default {
   namespaced: true,
   state: {
     months: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
+      { text: "January", value: 1 },
+      { text: "February", value: 2 },
+      { text: "March", value: 3 },
+      { text: "April", value: 4 },
+      { text: "May", value: 5 },
+      { text: "June", value: 6 },
+      { text: "July", value: 7 },
+      { text: "August", value: 8 },
+      { text: "September", value: 9 },
+      { text: "October", value: 910 },
+      { text: "November", value: 911 },
+      { text: "December", value: 912 }
     ]
   },
 
   getters: {
     getComputerMonthName(state) {
       return month => {
-        return state.months[month].toLowerCase();
+        return state.months[month].text.toLowerCase();
       };
     },
 
     getDisplayMonthName(state) {
       return month => {
-        return state.months[month];
+        return state.months[month].text;
       };
+    },
+
+    getAllDisplayMonthName(state) {
+      return state.months;
     }
   }
 };
