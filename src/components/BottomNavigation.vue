f04952d6c81aeadd08d6ed25bf31235de32af3f8
Lord Sayur <umar_317@hotmail.com>
2021-04-24 13:16:31 +0800

    ✨add floating menu button

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -1,26 +1,48 @@
 <template>
-  <v-bottom-navigation
-    v-model="selectedDistrict"
-    app
-    horizontal
-    grow
-    background-color="blue-grey darken-2"
-    dark
-    color="yellow"
-  >
-    <v-btn
-      v-for="(district, index) in districts"
-      :key="index"
-      height="100%"
-      :value="district.value"
-      @click="updateSelectedDistrict(district.value)"
+  <section id="bottom-nav">
+    <!-- Menu -->
+    <v-speed-dial
+      v-model="menu"
+      bottom
+      right
+      direction="top"
+      :open-on-hover="false"
+      transition="slide-y-reverse-transition"
     >
-      <span v-html="district.name"></span>
-    </v-btn>
-    <v-btn text icon @click="refresh">
-      <v-icon>refresh</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+      <template v-slot:activator>
+        <v-btn v-model="menu" color="blue-grey darken-2" dark fab small>
+          <v-icon v-if="menu" small color="grey">
+            close
+          </v-icon>
+          <v-icon v-else small color="grey">
+            menu
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="green" @click="refresh">
+        <v-icon>refresh</v-icon>
+      </v-btn>
+    </v-speed-dial>
+    <v-bottom-navigation
+      v-model="selectedDistrict"
+      app
+      horizontal
+      grow
+      background-color="blue-grey darken-2"
+      dark
+      color="yellow"
+    >
+      <v-btn
+        v-for="(district, index) in districts"
+        :key="index"
+        height="100%"
+        :value="district.value"
+        @click="updateSelectedDistrict(district.value)"
+      >
+        <span v-html="district.name"></span>
+      </v-btn>
+    </v-bottom-navigation>
+  </section>
 </template>
 
 <script>
@@ -35,6 +57,7 @@ export default {
 
   data() {
     return {
+      menu: false,
       selectedDistrict: "brunei",
       districts: [
         {
@@ -77,3 +100,17 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+/* This is for documentation purposes and will not be needed in your application */
+#bottom-nav .v-speed-dial {
+  z-index: 9999;
+  position: absolute;
+  bottom: 5rem;
+  right: 0;
+}
+
+#bottom-nav .v-btn--floating {
+  position: relative;
+}
+</style>
